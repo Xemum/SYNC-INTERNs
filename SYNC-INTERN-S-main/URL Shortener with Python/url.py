e3a872650768597cae8e83508516b75299b85c69
youcef mhammdi bouzina <112803317+Xemum@users.noreply.github.com>
2024-03-25 23:26:36 +0100

    m

--- SYNC-INTERN-S-main/URL Shortener with Python/url.py
+++ SYNC-INTERN-S-main/URL Shortener with Python/url.py
@@ -1,1 +1,13 @@
-print("hello world this is the start of the program")
+import pyshorteners
+
+# Initialize the Shortener object
+s = pyshorteners.Shortener()
+
+# Take the long URL as input
+long_url = input("Enter the long URL: ")
+
+# Get the shortened URL
+short_url = s.tinyurl.short(long_url)
+
+# Print the shortened URL
+print("Shortened URL:", short_url)
